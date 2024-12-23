b9392caaa6506c727d52b632e568fbc80bbbeb1c
cascadesite <hawktuahskibidisigma69420@gmail.com>
2024-12-22 23:02:24 -0500

    e

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
-particlesJS('particles-js', {
+// Eruda
+function loadEruda() {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/eruda';
+    document.body.appendChild(script);
+    script.onload = function () {
+        eruda.init();
+    };
+}
+
+(function () {
+    var keyword = '';
+    document.addEventListener('keypress', function (event) {
+        keyword += event.key.toLowerCase();
+        if (keyword.endsWith('eruda')) {
+            loadEruda();
+            keyword = '';
+        }
+        if (keyword.length > 5) {
+            keyword = keyword.slice(-5);
+        }
+    });
+})();
+// Random Phrase
+async function getRandomPhrase() {
+    try {
+      const response = await fetch('phrases.json');
+      const phrases = await response.json();
+      
+      const ultraRareIndex = phrases.length - 1;
+      const randomNumber = Math.random();
+      
+      if (randomNumber <= 0.01) {
+        return phrases[ultraRareIndex];
+      } else {
+        const randomIndex = Math.floor(Math.random() * (phrases.length - 1));
+        return phrases[randomIndex];
+      }
+    } catch (error) {
+      console.error('Error fetching phrases:', error);
+      return "An error occurred. Please try again.";
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', async () => {
+    const phraseContainer = document.getElementById('phrase-container');
+    const randomPhrase = await getRandomPhrase();
+    phraseContainer.textContent = randomPhrase;
+  });
+  // Particles
+  particlesJS('particles-js', {
     "particles": {
         "number": {
             "value": 100,
